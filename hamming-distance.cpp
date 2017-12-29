afcbef2f73d8132f7eb2106beba5e521f01b5715
amildie <amildie@gmail.com>
2017-12-28 22:08:00 -0300

    hamming distance

---
+++ hamming-distance.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int hammingDistance(int x, int y) {  
+      unsigned int hamming = 0;
+      const unsigned int MAX_LENGTH = 32;
+
+      std::string x_b = std::bitset<MAX_LENGTH>(x).to_string();
+      std::string y_b = std::bitset<MAX_LENGTH>(y).to_string();
+      
+      for(int i = 0; i < MAX_LENGTH; ++i) {
+        if(x_b[i] != y_b[i]) hamming++;
+      }
+
+      return hamming;
+    }
+};
