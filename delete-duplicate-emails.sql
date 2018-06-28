100f1b622e92b9b9dcebd9ccab7feab439da51f2
amildie <amildie@gmail.com>
2018-06-27 22:20:37 -0300

    delete-duplicate-emails

---
+++ delete-duplicate-emails.sql
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/delete-duplicate-emails/
+
+DELETE FROM Person
+WHERE Id NOT IN 
+    (SELECT * 
+     FROM 
+        (SELECT MIN(Id) FROM Person GROUP BY Email) 
+     AS t)
